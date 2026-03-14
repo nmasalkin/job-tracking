0d92e138024ec8dafffb3a3d5d84a1bfa49f1d3b
nmasalkin <nmasalkin96@yandex.ru>
2026-03-14 16:20:58 +0300

    Исправлен Main класс

--- src/main/java/ru/vk/education/job/Main.java
+++ src/main/java/ru/vk/education/job/Main.java
@@ -1,9 +1,9 @@
 package ru.vk.education.job;
 
 import ru.vk.education.job.app.cli.CLI;
-import ru.vk.education.job.app.service.impl.JobServiceImpl;
-import ru.vk.education.job.app.service.impl.MatchServiceImpl;
-import ru.vk.education.job.app.service.impl.UserServiceImpl;
+import ru.vk.education.job.app.service.JobService;
+import ru.vk.education.job.app.service.MatchService;
+import ru.vk.education.job.app.service.UserService;
 import ru.vk.education.job.data.repository.JobRepository;
 import ru.vk.education.job.data.repository.UserRepository;
 
@@ -12,9 +12,9 @@ public class Main {
         JobRepository jobRepository = new JobRepository();
         UserRepository userRepository = new UserRepository();
 
-        JobService jobService = new JobServiceImpl(jobRepository);
-        UserService userService = new UserServiceImpl(userRepository);
-        MatchService matchService = new MatchServiceImpl(userRepository, jobRepository);
+        JobService jobService = new JobService(jobRepository);
+        UserService userService = new UserService(userRepository);
+        MatchService matchService = new MatchService(userRepository, jobRepository);
 
         CLI cli = new CLI(userService, jobService, matchService);
         cli.readCommands();
